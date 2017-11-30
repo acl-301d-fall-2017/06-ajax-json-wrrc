2d3d675e0de2b9b460742308a4bf5712658261ce
Jack <lomax15@me.com>
2017-11-30 14:04:32 -0800

    completed some review and comment lines in the article.js folder

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -9,19 +9,20 @@ function Article (rawDataObj) {
     this.publishedOn = rawDataObj.publishedOn;
 }
 
-// REVIEW: Instead of a global `articles = []` array, let's attach this list of all articles directly to the constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves objects, which means we can add properties/values to them at any time. In this case, the array relates to ALL of the Article objects, so it does not belong on the prototype, as that would only be relevant to a single instantiated Article.
+// [X] REVIEW: Instead of a global `articles = []` array, let's attach this list of all articles directly to the constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves objects, which means we can add properties/values to them at any time. In this case, the array relates to ALL of the Article objects, so it does not belong on the prototype, as that would only be relevant to a single instantiated Article.
 Article.all = [];
 
-// COMMENT: Why isn't this method written as an arrow function?
-// PUT YOUR RESPONSE HERE
+// [X] COMMENT: Why isn't this method written as an arrow function?
+// If we were to create this as an arrow function the .this would relate to the window but it wouldn't relate to the object of this function.
+
 Article.prototype.toHtml = function() {
     const template = Handlebars.compile($('#article-template').text());
 
     this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
 
-    // COMMENT: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
+    // [X] COMMENT: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
     // Not sure? Check the docs!
-    // PUT YOUR RESPONSE HERE
+    // This is giving us the publishing information. The ${this.daysAgo} is the expression that produces the days of publication. It then renders that information on the site. We used these on tuesday to return the value of an author within the author filter. 
     this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
     this.body = marked(this.body);
 
@@ -33,7 +34,7 @@ Article.prototype.toHtml = function() {
 // REVIEW: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
 
 // COMMENT: Where is this function called? What does 'rawData' represent now? How is this different from previous labs? Where did our forEach loop that looped through all articles and called .toHtml() move to?
-// PUT YOUR RESPONSE HERE
+// This function is called within the Article.fetchAll. 'rawData' now represents the data in hackerlpsum.json folder. We are creating rawData as a JSON object now. The forEach is moved below to the push method in JSON.
 Article.loadAll = rawData => {
     rawData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)));
 
